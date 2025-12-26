0a0dfb891c7c0f6a90f362a7d4e3d65730168d12
mikele19 <micheleborra30@gmail.com>
2025-12-26 23:09:27 +0100

    hard reset globals css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,9 @@
-import type { Metadata } from "next";
-import { Baloo_2 } from "next/font/google";
 import "./globals.css";
-
-const baloo = Baloo_2({
-  subsets: ["latin"],
-  variable: "--font-baloo",
-});
-
-export const metadata: Metadata = {
-  title: "CatPacks",
-  description: "CatPacks",
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="it">
-      <body className={`${baloo.variable} antialiased`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
