472bd93187192dea0a6420ee141b149601da6c7e
mikele19 <micheleborra30@gmail.com>
2025-12-28 16:19:54 +0100

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+import HomeScreen from "@/app/components/screens/HomeScreen";
+
 export default function Page() {
-  return <div>OK</div>;
+  return <HomeScreen />;
 }
